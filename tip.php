c532ba65b4fd687b5d613049800f51704c121bf8
Sushant Magoo <sushantmagoo@gmail.com>
2017-01-22 06:41:47 +0530

    added return in tip.php

--- tip.php
+++ tip.php
@@ -3,35 +3,36 @@ $bill = $_GET['bill'];
 $percent = $_GET['percent'];
 $split = $_GET['split'];
 $tip = $bill * $percent;
-$subtotal = $bill + $tip;
+$total = $bill + $tip;
 if ($split !=0 ) {
-	$total = ($bill + $tip) / $split;
+	$perPerson = ($bill + $tip) / $split;
 } else {
-	$total = $bill + $tip;
+	$perPerson = $total;
 }
 ?>
 
 <html>
-	<head>
-		<title>Web Security Test</title>
-		<style type="text/css">
-  		body {
-    		font-family: monospace;
-    		text-align: center;
-    		background-color: #00FFFF;
-  		}
-  		</style>
+  <head>
+  	<title>Web Security Test</title>
+  	  <style type="text/css">
+  	  body {
+    	font-family: monospace;
+    	text-align: center;
+    	background-color: #00FFFF;
+  	  }
+  	  </style>
 	</head>
 	<body>
- 		<h1>Payment</h1>
- 		<p> <b>Tip: </b>
- 			<b><?php echo number_format($tip, 2); ?> USD</b>
- 		</p>
- 		<p> <b>Subtotal: </b>
- 			<b><?php echo number_format($subtotal, 2); ?> USD</b>
- 		</p>
- 		<p> <b>Total per person: </b>
- 			<b><?php echo number_format($total, 2); ?> USD</b>
- 		</p>
+      <h1>Your Payment</h1>
+      <p> <b>Tip: </b>
+        <b><?php echo number_format($tip, 2); ?> USD</b>
+      </p>
+      <p> <b>Total: </b>
+        <b><?php echo number_format($total, 2); ?> USD</b>
+      </p>
+      <p> <b>Total per person: </b>
+        <b><?php echo number_format($perPerson, 2); ?> USD</b>
+      </p>
+ 	  <a href="index.php"><b>RESET<b></a>
  	</body>
 </html>
